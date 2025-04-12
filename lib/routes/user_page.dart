eb57ccc6af77ae4388c2958a3f75064d2c33952e
ElifDK <elif.koc@sabanciuniv.edu>
2025-04-12 16:02:36 +0300

    Starter App

---
+++ lib/routes/user_page.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class UserPage extends StatefulWidget {
+  const UserPage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<UserPage> createState() => _UserPageState();
+}
+
+class _UserPageState extends State<UserPage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: Text(widget.title),
+          centerTitle: true,
+        ),
+        body: Column(
+            children:[
+              Expanded(child: Text(''))]
+            ));
+  }
+}
